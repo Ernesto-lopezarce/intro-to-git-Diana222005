470475443025ebd88564cb1d89002bec3bc43b8a
Diana222005 <diana.vazquez1995@alumnos.udg.mx>
2026-05-06 08:19:58 -0600

    primer commit Diana

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+int main()
+{
+    int edad;
+    printf("Indique su edad:");
+    scanf("%d",&edad);
+    if (edad>=18)
+        printf("Eres mayor de edad");
+    else
+        printf("Te falta vivir bb");
+    return 0;
+}
